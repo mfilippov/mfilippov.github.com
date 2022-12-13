3d357ff6c362c3ac9daca7c0a7200b87fc90198a
Mikhail Filippov <mikhail@filippov.me>
2022-12-13 12:36:45 +0400

    Update sitemap

--- update-sitemap.sh
+++ update-sitemap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-DATE=$(date -I seconds)
+DATE=$(date -Iseconds)
 sed -i '' -e "s/\<lastmod\>.*\<\/lastmod>/\<lastmod\>$DATE\<\/lastmod\>/g" "$(cd "$(dirname "$0")"; pwd)/sitemap.xml"
